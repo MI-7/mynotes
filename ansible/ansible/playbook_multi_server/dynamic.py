da11c731acca5b66cf79a55bc4b01a7ebf015c18
陈亮 <chenliang@chenliangdeiMac.local>
2018-02-17 11:45:44 +0800

    ..

--- ansible/ansible/playbook_multi_server/dynamic.py
+++ ansible/ansible/playbook_multi_server/dynamic.py
@@ -1,17 +1,56 @@
 #!/usr/bin/env python3.6
-import subprocess
+import argparse
+import json
+import sys
 import paramiko
 import os
 
-cmd = "vagrant ssh-config vagrant2"
-
 # p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
 
-textlist = os.popen(cmd).readlines()
-for line in textlist:
-    line = line.replace('\n', '')
-    print(line)
+def parse_args():
+    parser = argparse.ArgumentParser(description="Vagrant inventory script")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--list', action='store_true')
+    group.add_argument('--host')
+    return parser.parse_args()
 
-config = paramiko.SSHConfig()
-config.parse(textlist)
-print(config.lookup("vagrant2"))
+def list_running_hosts():
+    cmd = "vagrant status --machine-readable"
+    status = os.popen(cmd).readlines()
+    hosts = []
+    for line in status:
+        line = line.rstrip('\n')
+        host_status = line.split(',')
+        if host_status[2] == 'state' and host_status[3] == 'running':
+            hosts.append(host_status[1])
+    return hosts
+
+def get_host_details(host):
+    cmd = "vagrant ssh-config {}".format(host)
+
+    textlist = os.popen(cmd).readlines()
+    #for line in textlist:
+        #line = line.replace('\n', '')
+        #print(line)
+    
+    config = paramiko.SSHConfig()
+    config.parse(textlist)
+    c = config.lookup(host)
+    
+    return {'ansible_ssh_host': c['hostname'],
+            'ansible_ssh_port': c['port'],
+            'ansible_ssh_user': c['user'],
+            'ansible_ssh_private_key_file': c['identityfile'][0]}
+
+def main():
+    args = parse_args()
+    
+    if args.list:
+        hosts = list_running_hosts()
+        json.dump({'vagrant': hosts}, sys.stdout)
+    else:
+        details = get_host_details(args.host)
+        json.dump(details, sys.stdout)
+
+if __name__ == '__main__':
+    main()
